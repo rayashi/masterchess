d8026e516ca89abd925b278fa52d81b488b407f3
Humberto Dib Rayashi <rayashi@gmail.com>
2020-01-25 18:19:28 -0300

    Board and Cell components refactored

--- src/board/Board.js
+++ src/board/Board.js
@@ -1,30 +1,19 @@
-import React, { useState , useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 
 import Cell from "./Cell";
-import { setCells } from "./BoardActions";
 
 import './board.css';
 
 export default function Board() {
-    const dispatch = useDispatch();
     const cells = useSelector(state => state.BoardReducer.cells);
-
-    function onCellPress(clickedCell) {
-        const newCells = cells.map(line => 
-            line.map(cell => 
-                cell.id === clickedCell.id ? {...cell, knight: true} : {...cell, knight: false}
-            )
-        );
-        dispatch(setCells(newCells));
-    }
 
     return(
         <div id="board">
             {cells.map((line, lineIndex) =>
                 <div key={lineIndex} className="line">
                     {line.map(cell => 
-                        <Cell key={cell.id} cell={cell} onCellPress={onCellPress}/>
+                        <Cell key={cell.id} cell={cell}/>
                     )}
                 </div>
             )}
--- src/board/Cell.js
+++ src/board/Cell.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import classNames from 'classnames';
+import { useSelector, useDispatch } from "react-redux";
+import { setKnight } from "./BoardActions";
 
 export default function Cell(props) {
+    const dispatch = useDispatch();
+    const knight = useSelector(state => state.BoardReducer.knight);
+
+    const styleClasses = [
+        "cell",
+        props.cell.className,
+        knight && props.cell.id === knight.id && "knightCell"
+    ];
 
     function onCellPress() {
-        props.onCellPress(props.cell);
+        dispatch(setKnight(props.cell));
     }
 
     return(
-        <div onClick={onCellPress} 
-            className={classNames(["cell", props.cell.className, props.cell.knight && "knightCell"])}>            
-        </div>
+        <div onClick={onCellPress} className={classNames(styleClasses)} />
     )
 }
